be7cae1e5afe0c07e36e1283656d8014a1242eb6
Katsiaryna Ramenchyk <katar@MacBook-Air--Remenchyk.local>
2025-10-23 18:47:43 +0300

    init commit

---
+++ src/main/java/com/innowise/internship/InnowiseInternshipPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseInternshipPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseInternshipPaymentServiceApplication.class, args);
+    }
+
+}
